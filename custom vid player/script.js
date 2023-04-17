b41cb7f2c568d6fb28b93985ee45f20bf9f3b42c
AtharvaKarkar <93367238+AtharvaKarkar@users.noreply.github.com>
2023-04-18 01:17:57 +0530

    js and css file updated

---
+++ custom vid player/script.js
@@ -0,0 +1,29 @@
+const player=document.querySelector('player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method] ();
+}
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    console.log(icon);
+    toggle.textContent = icon;
+}
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+function handleProgress () {
+    
+}
+
