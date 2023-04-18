720eb8ecd92d8bfa08ce6c47bd7c96c3b6d4deb9
AtharvaKarkar <93367238+AtharvaKarkar@users.noreply.github.com>
2023-04-18 19:12:01 +0530

    added notes in html and js file

--- custom vid player/script.js
+++ custom vid player/script.js
@@ -9,8 +9,12 @@ const ranges = player.querySelectorAll('.player__slider');
 
 /* Build out functions */
 function togglePlay() {
-  const method = video.paused ? 'play' : 'pause';
-  video[method]();
+  if(video.paused) {
+    video.play();
+  }
+  else{
+    video.pause();
+  }
 }
 
 function updateButton() {
@@ -20,35 +24,37 @@ function updateButton() {
 }
 
 function skip() {
- video.currentTime += parseFloat(this.dataset.skip);
+ video.currentTime += parseFloat(this.dataset.skip);      // parseFloat is used to convert the data-skip in string format to number .
 }
 
 function handleRangeUpdate() {
   video[this.name] = this.value;
 }
-
+// updating the widht of progress bar by using flex-basis. We can control it's width by changing the flex-basis property.ALl we need to do is calculate the $ by dividing the current time by the duration.
 function handleProgress() {
   const percent = (video.currentTime / video.duration) * 100;
   progressBar.style.flexBasis = `${percent}%`;
 }
 
-function scrub(e) {
+function scrub(e) {   
+    // click and drag the progress bar to update playback time.
   const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
   video.currentTime = scrubTime;
+  // offsetX property tells how far your mouse is away from the edge of the target element, &  the offsetWidth property returns the layout width of the element.
 }
 
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
-video.addEventListener('timeupdate', handleProgress);
+video.addEventListener('timeupdate', handleProgress);  // whenever the playback time is updated, the width of the progress bar will be updated too.
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
-let mousedown = false;
+let mousedown = false; // created a flag variable to track if the usr if clicking on the mouse, bcz we only want the playback time to update when the user is actively doing it.
 progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
