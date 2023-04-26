a6746260b120cdc4c5cc71675b0605a32f8586b9
AtharvaKarkar <93367238+AtharvaKarkar@users.noreply.github.com>
2023-04-26 23:40:47 +0530

    Update app.js

--- Camera Fun19/app.js
+++ Camera Fun19/app.js
@@ -16,3 +16,27 @@ function getVideo() {
         console.error('OH SHIT !!',err);
     });
 }
+
+function painttoCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+    
+
+   return setInterval(() =>{
+    ctx.drawImage(video,0,0,width,height);
+    // take the pixels at
+    let pixels = ctx.getImageData(0,0,width,height);
+    // create a mess with them
+    // pixels = redEffect(pixels);
+
+    pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
+
+    // pixels = greenScreen(pixels);
+    // put them back
+    ctx.putImageData(pixels,0,0);
+   
+   }, 16);
+}
